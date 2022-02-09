095136aecf3d971e3685f61575ca06249a644a09
Arthur Sarmento <arthurbs17@gmail.com>
2022-02-08 21:12:33 -0300

    Read table function and exceptions done

--- app/controllers/vaccines_controller.py
+++ app/controllers/vaccines_controller.py
@@ -2,20 +2,23 @@ from flask import request, current_app, jsonify
 from app.models.vaccine_cards_model import VaccineCardModel
 from http import HTTPStatus
 from datetime import datetime, timedelta
+from app.exc.wrong_key_received import WrongKeyReceived
+from sqlalchemy.exc import IntegrityError, DataError
+
+keys_names = ["cpf", "name", "vaccine_name", "health_unit_name"]
 
 def create_vaccine_card():
     data = request.get_json()
+        
+    for key in keys_names:
+        if type(data[key]) != str:
+            return jsonify({"message": "Os valores devem ser passados em string"}), HTTPStatus.BAD_REQUEST
 
     try:
         new_vaccine = VaccineCardModel(**data)
         
         if new_vaccine.cpf.isdigit() == False:
-            return jsonify({"message": "Passar apenas valores numéricos!"}), HTTPStatus.BAD_REQUEST
-
-        values = [value for value in data.values()]
-        for value in values:
-            if type(value) in values != str:
-                return jsonify({"message": "Os valores devem ser passados em string"}), HTTPStatus.BAD_REQUEST
+            return jsonify({"message": "Passar apenas valores numéricos no CPF!"}), HTTPStatus.BAD_REQUEST
 
         new_vaccine.cpf = new_vaccine.cpf
         new_vaccine.name = new_vaccine.name.title()
@@ -29,5 +32,19 @@ def create_vaccine_card():
 
         return jsonify(new_vaccine), HTTPStatus.CREATED
         
-    except KeyError:
-        return {"message": "Chave errada!"}
+    except TypeError:
+        error = WrongKeyReceived(data)
+        return jsonify(error.message), HTTPStatus.BAD_REQUEST
+    except AttributeError:
+        error = WrongKeyReceived(data)
+        return jsonify(error.miss_keys), HTTPStatus.BAD_REQUEST
+    except IntegrityError:
+        return jsonify({"message": "CPF já cadastrado!"}), HTTPStatus.CONFLICT
+    except DataError:
+        return jsonify({"message": "CPF com mais de 11 números"}), HTTPStatus.BAD_REQUEST
+
+def read_vaccine():
+    
+    vaccines_list = current_app.db.session.query(VaccineCardModel).all()
+
+    return jsonify(vaccines_list), HTTPStatus.OK
--- app/exc/wrong_key_received.py
+++ app/exc/wrong_key_received.py
@@ -7,7 +7,15 @@ class WrongKeyReceived(Exception):
             "available_keys": self.correct_keys,
             "wrong_keys": self.wrong_key(data)
         }
+        self.miss_keys = {
+            "available_keys": self.correct_keys,
+            "missing_keys": self.missing_key(data)
+        }
 
     @classmethod
     def wrong_key(cls, data: dict) -> list:
         return [key for key in data.keys() if key not in cls.correct_keys]
+    
+    @classmethod
+    def missing_key(cls, data: dict) -> list:
+        return [key for key in cls.correct_keys if key not in data.keys()]
